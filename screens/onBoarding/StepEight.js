e1678f22ed635c86b100dd4985f69a2dcff98a76
omerad1 <omerad@post.bgu.ac.il>
2025-01-10 16:09:09 +0200

    step eight

---
+++ screens/onBoarding/StepEight.js
@@ -0,0 +1,21 @@
+import OnBoardingLayout from "../../components/onBoarding/OnBoardingLayout";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import InputField from "../../components/onBoarding/InputField";
+import { useState } from "react";
+const StepEight = () => {
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  return (
+    <OnBoardingLayout
+      direction="StepTwo"
+      next={true}
+      title={`Now, You'll answer a few questions to help others get to know you better and to make the matching process more accurate and personalized.
+        Feel free to skip any questions at any time!`}
+    ></OnBoardingLayout>
+  );
+};
+
+export default StepEight;
+
+const styles = StyleSheet.create({});
