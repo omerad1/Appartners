30ff291e0e44e94c115711db487db33d158fcbc7
Ronohayon27 <127681444+Ronohayon27@users.noreply.github.com>
2025-06-05 21:34:57 +0300

    fixing movin on

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -172,7 +172,7 @@ const LoginScreen = () => {
             <Text style={styles.signUpText}>Don't have an account? </Text>
             <TouchableOpacity
               onPress={() =>
-                navigation.navigate("OnBoarding", { screen: "register" })
+                navigation.navigate("OnBoarding", { screen: "StepOne" })
               }
             >
               <Text style={styles.signUpLink}>Sign up</Text>
